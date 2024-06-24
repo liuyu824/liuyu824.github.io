655f801216691260701866fb9c1a74afa464ff3e
liuyu98824 <liuyu11@baicgroup.com.cn>
2024-06-24 15:46:21 +0800

    update

---
+++ communistExamPlatform/src/main/java/org/liuyu/communistexamplatform/CommunistExamPlatformApplication.java
@@ -0,0 +1,13 @@
+package org.liuyu.communistexamplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunistExamPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunistExamPlatformApplication.class, args);
+    }
+
+}
